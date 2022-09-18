49c4a6a6e2c65b77da58f3554941b95e183cbbae
Chris Oh <86143600+jechangoh@users.noreply.github.com>
2022-09-17 23:23:56 -0400

    Add files via upload

---
+++ open.js
@@ -0,0 +1,27 @@
+function Open() {
+    var articles = document.getElementById("more_articles");
+    var button = document.getElementById("close_btn");
+    articles.style.display = "flex";
+    button.style.display = "none";
+}
+            
+function Open2() {
+    var articles = document.getElementById("more_articles2");
+    var button = document.getElementById("close_btn2");
+    articles.style.display = "flex";
+    button.style.display = "none";
+}
+
+function Open3() {
+    var articles = document.getElementById("more_articles3");
+    var button = document.getElementById("close_btn3");
+    articles.style.display = "flex";
+    button.style.display = "none";
+}
+
+function Open4() {
+    var articles = document.getElementById("more_articles4");
+    var button = document.getElementById("close_btn4");
+    articles.style.display = "flex";
+    button.style.display = "none";
+}
